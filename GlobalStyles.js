45566effb494fec8670b5f7178cc5e8c16c90855
SONIA <107837628+SONIA1415@users.noreply.github.com>
2024-02-29 14:48:34 +0530

    Add files via upload

---
+++ GlobalStyles.js
@@ -0,0 +1,33 @@
+/* fonts */
+export const FontFamily = {
+  font: "font",
+  arial: "Arial",
+  interRegular: "Inter-Regular",
+  timesNewRoman: "Times New Roman",
+};
+/* font sizes */
+export const FontSize = {
+  size_5xl: 24,
+  size_sm: 14,
+  size_base: 16,
+  size_xl: 20,
+  size_xs: 12,
+};
+/* Colors */
+export const Color = {
+  colorWhite: "#fff",
+  colorBlack: "#000",
+  colorDarkgray: "#989898",
+  colorNavajowhite_100: "rgba(251, 220, 157, 0.4)",
+  colorDarkgoldenrod: "#bc8310",
+  colorGoldenrod_100: "#ebbb5c",
+  colorGoldenrod_200: "#e7b95c",
+  colorDarkslategray: "#404040",
+  colorPapayawhip: "#fdf1d8",
+};
+/* border radiuses */
+export const Border = {
+  br_xs: 12,
+  br_11xs: 2,
+  br_6xl: 25,
+};
